7d965caa61011716b7055c605cbd9eb46fa7c73b
Roman <romagritcaenko@gmail.com>
2024-12-09 21:18:23 +0200

    parse config

--- src/config.rs
+++ src/config.rs
@@ -1,6 +1,8 @@
 use core::fmt;
 use dirs;
+use serde::Deserialize;
 use std::{fs, io, path::PathBuf};
+use toml::{self, de};
 
 pub const APP_NAME: &str = "Weblib";
 const CONFIG_FILE_NAME: &str = "config.toml";
@@ -13,6 +15,7 @@ fn get_config_path() -> Option<PathBuf> {
     Some(config_path)
 }
 
+#[derive(Debug, Deserialize)]
 pub struct Config {
     vault_path: Option<PathBuf>,
 }
@@ -24,11 +27,13 @@ impl Default for Config {
 
 pub fn get_config() -> Result<Config, Error> {
     let config_path = get_config_path().ok_or(Error::GetConfigPathFailed)?;
+    dbg!(&config_path);
 
-    if fs::exists(config_path)? {
-        todo!();
-        // let vault_path = Some(PathBuf::from("foo"));
-        // Ok(Config { vault_path })
+    if fs::exists(&config_path)? {
+        let content = fs::read_to_string(config_path)?;
+        let config: Config = toml::from_str(&content)?;
+
+        Ok(config)
     } else {
         Ok(Config::default())
     }
@@ -39,17 +44,24 @@ pub fn get_config() -> Result<Config, Error> {
 pub enum Error {
     GetConfigPathFailed,
     IoError(io::Error),
+    ParseError(de::Error),
 }
 impl fmt::Display for Error {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
             Error::GetConfigPathFailed => write!(f, "Failed to get the config path"),
             Error::IoError(io_error) => write!(f, "Io Error: {}", io_error),
+            Error::ParseError(parse_error) => write!(f, "TOML Parse Error: {}", parse_error),
         }
     }
 }
 impl From<io::Error> for Error {
     fn from(io_error: io::Error) -> Self {
         Error::IoError(io_error)
+    }
+}
+impl From<de::Error> for Error {
+    fn from(de_error: de::Error) -> Self {
+        Error::ParseError(de_error)
     }
 }
